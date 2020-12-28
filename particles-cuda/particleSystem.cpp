d80da3e890aff404b2219c1fde5ab9331e0e9346
MK <me@yomama.com>
2020-12-28 14:36:01 +0300

    -

--- particles-cuda/particleSystem.cpp
+++ particles-cuda/particleSystem.cpp
@@ -105,28 +105,6 @@ inline float lerp(float a, float b, float t)
     return a + t*(b-a);
 }
 
-// create a color ramp
-void colorRamp(float t, float *r)
-{
-    const int ncolors = 7;
-    float c[ncolors][3] =
-    {
-        { 1.0, 0.0, 0.0, },
-        { 1.0, 0.5, 0.0, },
-        { 1.0, 1.0, 0.0, },
-        { 0.0, 1.0, 0.0, },
-        { 0.0, 1.0, 1.0, },
-        { 0.0, 0.0, 1.0, },
-        { 1.0, 0.0, 1.0, },
-    };
-    t = t * (ncolors-1);
-    int i = (int) t;
-    float u = t - floor(t);
-    r[0] = lerp(c[i][0], c[i+1][0], u);
-    r[1] = lerp(c[i][1], c[i+1][1], u);
-    r[2] = lerp(c[i][2], c[i+1][2], u);
-}
-
 void
 ParticleSystem::_initialize(int numParticles)
 {
